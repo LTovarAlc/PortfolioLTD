efa8aec26feec2b4fd17d45288b2b23d77dd56cd
Luis Tovar <130521074+LTovarAlc@users.noreply.github.com>
2023-11-10 11:32:02 -0600

    falta que funcione el formulario de contacto

--- Scripts/formAction.js
+++ Scripts/formAction.js
@@ -10,6 +10,6 @@ $form.addEventListener('submit', handleSubmit);
 function handleSubmit(event){
     event.preventDefault()
     const form = new FormData(this)
-    $buttonMailto.setAttribute('href', `mailto:ltovaralc@gmail.com?subject=${form.get('name')}${form.get('email')}&body=${form.get('message')}`)
+    $buttonMailto.setAttribute('href', `mailto:ltovaralc@gmail.com?subject= Name: ${form.get('name')} Email: ${form.get('email')} correo &body=${form.get('message')}`)
     $buttonMailto.click()
 }
