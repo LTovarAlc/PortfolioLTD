178712eb1942dd7ed46f420c02cdfc52575349b0
Luis Tovar <130521074+LTovarAlc@users.noreply.github.com>
2023-11-11 09:32:26 -0600

    formulario conectado a email

--- Scripts/formAction.js
+++ Scripts/formAction.js
@@ -2,14 +2,3 @@
 
 //Portfolio online: https://portfolio-luis-tovar-dev.vercel.app/ 
 
-const $form = document.querySelector('#form')
-const $buttonMailto = document.querySelector('#trick')
-
-$form.addEventListener('submit', handleSubmit);
-
-function handleSubmit(event){
-    event.preventDefault()
-    const form = new FormData(this)
-    $buttonMailto.setAttribute('href', `mailto:ltovaralc@gmail.com?subject= Name: ${form.get('name')} Email: ${form.get('email')} correo &body=${form.get('message')}`)
-    $buttonMailto.click()
-}
